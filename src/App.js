c80835175acb1e3787d4400f5513f61a5db60a15
Manu <meluvung@thoughtworks.com>
2021-04-14 13:13:52 +0200

    Code to enable server side rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+    const [showMessage, setShowMessage] = React.useState(false);
+    return (
+        <div className="App">
+            <h1>Hello world</h1>
+            <input type="button" onClick={() => setShowMessage(!showMessage)} value="Toggle message"/>
+            {showMessage && <div>I am a dynamically rendered message</div>}
+        </div>
+    );
 }
 
 export default App;
